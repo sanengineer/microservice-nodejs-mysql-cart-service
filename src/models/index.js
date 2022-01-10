2d13ae2ed0d508276d21874d4cd83c6e44bd4f04
sanengineer <48153104+sanengineer@users.noreply.github.com>
2022-01-10 23:46:28 +0700

    update code on models indexjs

--- src/models/index.js
+++ src/models/index.js
@@ -13,16 +13,16 @@ console.log(env);
 console.log("CONFIG", config);
 
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
-// }
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
